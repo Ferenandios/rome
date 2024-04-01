c961aa1edf071b688eb71a5a6ae46b8ac6306495
Ferenandios <www.terunplay@gmail.com>
2024-04-01 06:05:57 +0300

    Added MainPage-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import MainPage from "./components/MainPage/MainPage";
 
 const App: FC = (): JSX.Element => {
-  return <div>works!</div>;
+  return (
+    <>
+      <MainPage />
+    </>
+  );
 };
 
 export default App;
