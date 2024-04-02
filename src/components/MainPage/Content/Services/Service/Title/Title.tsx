83dc54441001b143491a55a4939fb351748f4f73
Ferenandios <www.terunplay@gmail.com>
2024-04-02 19:31:49 +0300

    Created Title-component in Service-component

---
+++ src/components/MainPage/Content/Services/Service/Title/Title.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { useAppSelector } from "../../../../../../hooks";
+
+const Title: FC<{ serviceId: number }> = ({ serviceId }): JSX.Element => {
+  const { title } = useAppSelector((state) => state.services[serviceId]);
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-[60px] bg-white overflow-hidden">
+        <h3 className="font-bold text-[14px] text-center max-h-full leading-[16px]">
+          {title}
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default Title;
