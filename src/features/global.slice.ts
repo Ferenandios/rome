fc61ba1c7a627d9d6107d76a78018131583e8004
Ferenandios <www.terunplay@gmail.com>
2024-04-02 18:33:36 +0300

    Added three new properties in state & Created two reducers for them

--- src/features/global.slice.ts
+++ src/features/global.slice.ts
@@ -1,14 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { type IState } from "../types/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IService, type IState } from "../types/types";
 
 const initialState: IState = {
   showedPage: "main",
+  search: "",
+  placeholder: "",
+  services: [
+    { id: 0, title: "Лазерный рескрой листового металла" },
+    { id: 1, title: "Порошковая покраска металлических изделий" },
+    { id: 2, title: "Изготовление металлоконструкций" },
+    { id: 3, title: "Гибка металла" },
+  ],
 };
 
 const globalSlice = createSlice({
   name: "global",
   initialState,
-  reducers: {},
+  reducers: {
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload;
+    },
+    setSearchPlaceholder: (state) => {
+      if (state.services.length <= 0) return;
+      const randomService: IService =
+        state.services[Math.floor(Math.random() * state.services.length)];
+      state.placeholder = randomService.title;
+    },
+  },
 });
 
 export default globalSlice.reducer;
+export const { setSearch, setSearchPlaceholder } = globalSlice.actions;
