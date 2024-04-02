8c37cff45500063f52dbd2722a046c2bb4b950c1
Ferenandios <www.terunplay@gmail.com>
2024-04-02 18:30:24 +0300

    Created Input-component

---
+++ src/components/Header/Search/Input/Input.tsx
@@ -0,0 +1,30 @@
+import { FC, useEffect } from "react";
+import {
+  setSearch,
+  setSearchPlaceholder,
+} from "../../../../features/global.slice";
+import { useAppDispatch, useAppSelector } from "../../../../hooks";
+
+const Input: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { placeholder, search } = useAppSelector((state) => state);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearch(event.target.value));
+  };
+  useEffect(() => {
+    dispatch(setSearchPlaceholder());
+  }, []);
+  return (
+    <>
+      <input
+        className="text-[12px] w-[90%] min-w-[28px] h-[28px] bg-white rounded-full pl-[8px] pr-[27px] drop-shadow-md text-ellipsis"
+        onChange={handleChange}
+        value={search}
+        placeholder={placeholder}
+        type="text"
+      />
+    </>
+  );
+};
+
+export default Input;
