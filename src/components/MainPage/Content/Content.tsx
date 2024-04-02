944a6767e91c6e37e61567484ceed29cd938ae3e
Ferenandios <www.terunplay@gmail.com>
2024-04-02 19:33:59 +0300

    Added Services-component

--- src/components/MainPage/Content/Content.tsx
+++ src/components/MainPage/Content/Content.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import Title from "./Title/Title";
+import Services from "./Services/Services";
 
 const Content: FC = (): JSX.Element => {
   return (
     <>
-      <section className="pt-[12px]">
+      <section className="pt-[12px] pb-[18px]">
         <div className="container px-[8px]">
-          <div>
+          <div className="flex flex-col gap-[28px]">
             <Title />
+            <Services />
           </div>
         </div>
       </section>
