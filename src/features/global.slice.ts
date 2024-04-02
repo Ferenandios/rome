efde9d705bdf7f322365988cd6290d50dfddd01d
Ferenandios <www.terunplay@gmail.com>
2024-04-02 19:33:40 +0300

    Added images for services in every element

--- src/features/global.slice.ts
+++ src/features/global.slice.ts
@@ -1,15 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IService, type IState } from "../types/types";
 
+import serviceImage0 from "/src/assets/Services/0.jpg";
+import serviceImage1 from "/src/assets/Services/1.jpg";
+import serviceImage2 from "/src/assets/Services/2.jpg";
+import serviceImage3 from "/src/assets/Services/3.jpg";
+
 const initialState: IState = {
   showedPage: "main",
   search: "",
   placeholder: "",
   services: [
-    { id: 0, title: "Лазерный рескрой листового металла" },
-    { id: 1, title: "Порошковая покраска металлических изделий" },
-    { id: 2, title: "Изготовление металлоконструкций" },
-    { id: 3, title: "Гибка металла" },
+    {
+      id: 0,
+      title: "Лазерный рескрой листового металла",
+      image: serviceImage0,
+    },
+    {
+      id: 1,
+      title: "Порошковая покраска металлических изделий",
+      image: serviceImage1,
+    },
+    { id: 2, title: "Изготовление металлоконструкций", image: serviceImage2 },
+    { id: 3, title: "Гибка металла", image: serviceImage3 },
   ],
 };
 
