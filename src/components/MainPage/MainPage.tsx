6138187e53e2fc2d523e540fd6fcc093e196b39f
Ferenandios <www.terunplay@gmail.com>
2024-04-02 21:04:17 +0300

    Added Footer-component

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Header from "../Header/Header";
 import Content from "./Content/Content";
+import Footer from "../Footer/Footer";
 
 const MainPage: FC = (): JSX.Element => {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 };
