405910b01d6191dfbf203649edcafdcbfe06f33f
Ferenandios <www.terunplay@gmail.com>
2024-04-02 17:31:11 +0300

    Added show-by-showedPage property from state for MainPage-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
 import MainPage from "./components/MainPage/MainPage";
+import { useAppSelector } from "./hooks";
 
 const App: FC = (): JSX.Element => {
-  return (
-    <>
-      <MainPage />
-    </>
-  );
+  const { showedPage } = useAppSelector((state) => state);
+  return <>{showedPage === "main" && <MainPage />}</>;
 };
 
 export default App;
