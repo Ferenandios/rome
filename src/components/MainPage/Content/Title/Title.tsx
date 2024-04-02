f545fac83376965ee36f6b497939c12c403355b3
Ferenandios <www.terunplay@gmail.com>
2024-04-02 18:57:51 +0300

    Created Title-component

---
+++ src/components/MainPage/Content/Title/Title.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Title: FC = (): JSX.Element => {
+  return (
+    <>
+      <h2 className="font-bold text-[18px] text-center flex justify-center min-h-[24px]">
+        Наш ассортимент
+      </h2>
+    </>
+  );
+};
+
+export default Title;
