8ec901f725226fec29ebad463d81a50bb39466e6
Ferenandios <www.terunplay@gmail.com>
2024-04-02 18:58:09 +0300

    Added Content-component

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import Header from "../Header/Header";
+import Content from "./Content/Content";
 
 const MainPage: FC = (): JSX.Element => {
   return (
     <>
       <Header />
+      <Content />
     </>
   );
 };
