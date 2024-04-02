c39e30d5f62d1765709658fe79dd69386c22fa9c
Ferenandios <www.terunplay@gmail.com>
2024-04-02 19:31:16 +0300

    Created Services-component

---
+++ src/components/MainPage/Content/Services/Services.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { useAppSelector } from "../../../../hooks";
+import Service from "./Service/Service";
+
+const Services: FC = (): JSX.Element => {
+  const { services } = useAppSelector((state) => state);
+  return (
+    <>
+      <div className="flex flex-col items-center gap-[12px]">
+        {services.map((service) => (
+          <Service key={service.id} serviceId={service.id} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Services;
