f335d3880890e90abdc85fb60624556aac97929e
Ferenandios <www.terunplay@gmail.com>
2024-04-03 11:50:27 +0300

    Created Title-component in Crumb-component

---
+++ src/components/Header/Burger/Crumb/Title/Title.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+const Title: FC<{ title: string }> = ({ title }) => {
+  // ! DO TITLE STYLES
+  // ! DO TITLE STYLES
+  // ! DO TITLE STYLES
+  // ! DO TITLE STYLES
+  return (
+    <>
+      <div>
+        <h3>{title}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Title;
