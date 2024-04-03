ada5e375d1c107af3090e6880270f64bc9ec4a36
Ferenandios <www.terunplay@gmail.com>
2024-04-03 06:06:02 +0300

    Removed one style in footer-tag & Added Logo-component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import Info from "./Info/Info";
+import Logo from "./Logo/Logo";
 
 const Footer: FC = (): JSX.Element => {
   return (
     <>
-      <footer className="pt-[24px] pb-[12px] px-[8px]">
+      <footer className="pt-[24px] pb-[12px]">
         <div className="container px-[8px]">
           <div>
             <Info />
+            <Logo />
           </div>
         </div>
       </footer>
