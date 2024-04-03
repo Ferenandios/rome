aa561cf52c762cdea111bf9dac992259db716a75
Ferenandios <www.terunplay@gmail.com>
2024-04-03 05:49:54 +0300

    Added styles in inner

--- src/components/Footer/Info/Links/Links.tsx
+++ src/components/Footer/Info/Links/Links.tsx
@@ -4,12 +4,11 @@ import Title from "./Title/Title";
 import Items from "./Items/Items";
 
 const Links: FC<IFooterLinksList> = ({ styles, title, text }): JSX.Element => {
-  // ! FIX styles.text
-  // ! FIX styles.text
-  // ! FIX styles.text
-  // ! FIX styles.text
   return (
-    <div style={{ textAlign: `${styles.text}` }}>
+    <div
+      className="flex flex-col gap-[4px]"
+      style={{ textAlign: `${styles.text}` }}
+    >
       <Title title={title} />
       <Items items={text} />
     </div>
