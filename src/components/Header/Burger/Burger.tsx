bd1201cfa116b05d77fc3581518cc402d3f78844
Ferenandios <www.terunplay@gmail.com>
2024-04-03 08:07:42 +0300

    Added toggle in Hamburger-component

--- src/components/Header/Burger/Burger.tsx
+++ src/components/Header/Burger/Burger.tsx
@@ -1,11 +1,21 @@
-import { FC } from "react";
 import Hamburger from "hamburger-react";
+import { FC } from "react";
+import { toggleBurgerIsOpen } from "../../../features/global.slice";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
 
 const Burger: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { burgerIsOpen } = useAppSelector((state) => state);
   return (
     <>
       <div className="flex items-center h-full">
-        <Hamburger direction="right" size={28} rounded />
+        <Hamburger
+          toggled={burgerIsOpen}
+          toggle={() => dispatch(toggleBurgerIsOpen())}
+          direction="right"
+          size={28}
+          rounded
+        />
       </div>
     </>
   );
