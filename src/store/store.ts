89efeb8eb864c9ce911abcec25f8a5e00595fd53
Ferenandios <www.terunplay@gmail.com>
2024-04-04 17:52:18 +0300

    Added one more reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import globalSlice from "../features/global.slice";
+import headerSlice from "../features/header.slice";
 
-export const store = configureStore({ reducer: globalSlice });
+export const store = configureStore({
+  reducer: { global: globalSlice, header: headerSlice },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
