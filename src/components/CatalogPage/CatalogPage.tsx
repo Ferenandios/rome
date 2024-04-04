807339922926a5ea8ab4d308659d516474fe9163
Ferenandios <www.terunplay@gmail.com>
2024-04-04 06:53:45 +0300

    Created CatalogPage

---
+++ src/components/CatalogPage/CatalogPage.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const CatalogPage: FC = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <>Catalog</>
+      <Footer />
+    </>
+  );
+};
+
+export default CatalogPage;
