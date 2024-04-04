9cc00875af98ef2d68232410602d57eb11bcfddb
Ferenandios <www.terunplay@gmail.com>
2024-04-04 18:20:06 +0300

    Added footer store

--- src/components/Footer/Info/Info.tsx
+++ src/components/Footer/Info/Info.tsx
@@ -2,38 +2,27 @@ import { FC } from "react";
 import Links from "./Links/Links";
 import { type IFooterLinksList } from "../../../types/types";
 import { useAppSelector } from "../../../hooks";
+import { titleType } from "../../../types/Footer/types";
+import Items from "./Links/Items/Items";
 
 const Info: FC = (): JSX.Element => {
-  const { contacts } = useAppSelector((state) => state.global);
-  const generateLinksListText = (): { text: string }[] =>
-    contacts.phones.concat(contacts.emails).map((e) => ({ text: e }));
-  const linksList: IFooterLinksList[] = [
-    {
-      title: "Категории",
-      text: [
-        { text: "Главная" },
-        { text: "Каталог оборудования" },
-        { text: "Обратная связь" },
-      ],
-      styles: { text: "left" },
-    },
-    {
-      title: "Контакты",
-      text: generateLinksListText(),
-      styles: { text: "right" },
-    },
-  ];
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  // ! DO UI FOR FOOTER IN FOOTER.SLICE.TS
+  const { categories, contacts } = useAppSelector((state) => state.footer);
+  const titles: titleType[] = ["Категории", "Контакты"];
   return (
     <>
       <div className="flex justify-between">
-        {linksList.map((links, index) => (
-          <Links
-            key={index}
-            title={links.title}
-            text={links.text}
-            styles={links.styles}
-          />
-        ))}
+        {titles.map((title) => (<Items  />))}
       </div>
     </>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import globalSlice from "../features/global.slice";
 import headerSlice from "../features/header.slice";
+import footerSlice from "../features/footer.slice";
 
 export const store = configureStore({
-  reducer: { global: globalSlice, header: headerSlice },
+  reducer: {
+    global: globalSlice,
+    header: headerSlice,
+    footer: footerSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
