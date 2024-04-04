3db44ef3205f3cd7c41787fb3f76f3ca2d71fb8a
Ferenandios <www.terunplay@gmail.com>
2024-04-04 17:51:03 +0300

    Updated useSelector

--- src/components/MainPage/Content/Services/Service/Title/Title.tsx
+++ src/components/MainPage/Content/Services/Service/Title/Title.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { useAppSelector } from "../../../../../../hooks";
 
 const Title: FC<{ serviceId: number }> = ({ serviceId }): JSX.Element => {
-  const { title } = useAppSelector((state) => state.services[serviceId]);
+  const { title } = useAppSelector((state) => state.global.services[serviceId]);
   return (
     <>
       <div className="flex justify-center items-center w-full h-[60px] bg-white overflow-hidden">
