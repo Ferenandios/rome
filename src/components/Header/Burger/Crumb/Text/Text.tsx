5306b7c2037f20f8f7e0e5f09acb4c6af6f29aa5
Ferenandios <www.terunplay@gmail.com>
2024-04-04 17:48:13 +0300

    Added NavLink

--- src/components/Header/Burger/Crumb/Text/Text.tsx
+++ src/components/Header/Burger/Crumb/Text/Text.tsx
@@ -1,16 +1,27 @@
 import { FC } from "react";
-import { ICrumbText } from "../../../../../types/types";
+import { NavLink } from "react-router-dom";
+import { toggleBurgerIsOpen } from "../../../../../features/global.slice";
+import { useAppDispatch } from "../../../../../hooks";
+import { type IElement } from "../../../../../types/Header/types";
 
-const Text: FC<ICrumbText> = ({ text, active }): JSX.Element => {
+const Text: FC<{ el: IElement }> = ({ el }): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(toggleBurgerIsOpen());
+  };
   return (
     <>
-      <p
-        className={`${
-          active && "font-semibold"
-        } text-[16px] w-[calc(100vw-34px)] overflow-hidden`}
+      <NavLink
+        onMouseUp={handleClick}
+        className={({ isActive, isPending }) =>
+          `${
+            isPending || (isActive && "font-semibold")
+          } text-[16px] w-[calc(100vw-34px)] overflow-hidden`
+        }
+        to={el.link}
       >
-        {text}
-      </p>
+        {el.text}
+      </NavLink>
     </>
   );
 };
