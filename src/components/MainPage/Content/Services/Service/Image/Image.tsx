9f9dcbc22518d63ffcc4a91c55296938ded2628d
Ferenandios <www.terunplay@gmail.com>
2024-04-04 17:50:50 +0300

    Updated useSelector

--- src/components/MainPage/Content/Services/Service/Image/Image.tsx
+++ src/components/MainPage/Content/Services/Service/Image/Image.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { useAppSelector } from "../../../../../../hooks";
 
 const Image: FC<{ serivceId: number }> = ({ serivceId }): JSX.Element => {
-  const { image } = useAppSelector((state) => state.services[serivceId]);
+  const { image } = useAppSelector((state) => state.global.services[serivceId]);
   return (
     <>
       <div className="w-full h-[200px]">
