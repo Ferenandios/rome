ee1ec8eafc2e62095368210834961677ebf054b9
Ferenandios <www.terunplay@gmail.com>
2024-04-04 11:09:01 +0300

    Added RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage.tsx";
+import "./index.css";
 import { store } from "./store/store.ts";
+import CatalogPage from "./components/CatalogPage/CatalogPage.tsx";
 
+const router = createBrowserRouter([
+  { path: "/", element: <MainPage /> },
+  { path: "/catalog", element: <CatalogPage /> },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
