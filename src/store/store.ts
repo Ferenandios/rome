bff64fa4c31eb87ba4b95be974155e498965e621
Ferenandios <www.terunplay@gmail.com>
2024-04-05 05:47:24 +0300

    Added mainContent reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import globalSlice from "../features/global.slice";
 import headerSlice from "../features/header.slice";
 import footerSlice from "../features/footer.slice";
+import mainContentSlice from "../features/mainContent.slice";
 
 export const store = configureStore({
   reducer: {
     global: globalSlice,
     header: headerSlice,
+    mainContent: mainContentSlice,
     footer: footerSlice,
   },
 });
