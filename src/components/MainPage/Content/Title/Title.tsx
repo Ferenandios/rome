8fc126751e5d2586ed3690750bb0d3a008b70b27
Ferenandios <www.terunplay@gmail.com>
2024-04-05 05:47:58 +0300

    Added title property from State

--- src/components/MainPage/Content/Title/Title.tsx
+++ src/components/MainPage/Content/Title/Title.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import { useAppSelector } from "../../../../hooks";
 
 const Title: FC = (): JSX.Element => {
+  const { title } = useAppSelector((state) => state.mainContent);
   return (
     <>
       <h2 className="font-bold text-[18px] text-center flex justify-center min-h-[24px]">
-        Наш ассортимент
+        {title}
       </h2>
     </>
   );
