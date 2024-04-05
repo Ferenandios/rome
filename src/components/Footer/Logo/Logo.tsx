8ab99ec5b92f6ad6ff02f87fcf516f8eb51a4fb9
Ferenandios <www.terunplay@gmail.com>
2024-04-05 05:48:24 +0300

    Added logoIcon property from State

--- src/components/Footer/Logo/Logo.tsx
+++ src/components/Footer/Logo/Logo.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
-import logoAsset from "/logo.jpg";
+import { useAppSelector } from "../../../hooks";
 
 const Logo: FC = (): JSX.Element => {
+  const { logoIcon } = useAppSelector((state) => state.footer);
   return (
     <>
       <div className="flex justify-center w-full h-[50px] mt-[24px]">
-        <img className="h-full" src={logoAsset} alt="Logo" />
+        <img className="h-full" src={logoIcon} alt="Logo" />
       </div>
     </>
   );
