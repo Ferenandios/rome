b9bb81749db48cd96d21d5347d442a166cefaafb
Ferenandios <www.terunplay@gmail.com>
2024-04-05 14:53:01 +0300

    Fixed background-color

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -13,8 +13,9 @@ const Message: FC = (): JSX.Element => {
     <>
       <button
         onMouseUp={handleClick}
+        style={{ backgroundColor: colorTheme }}
         className={`md:hidden fixed z-30 bottom-[15px] right-[15px] flex justify-center items-center 
-          w-[50px] h-[50px] bg-[${colorTheme}] rounded-full`}
+          w-[50px] h-[50px] rounded-full`}
       >
         <img className="w-[47%]" src={logoIcon} alt="Write" />
       </button>
