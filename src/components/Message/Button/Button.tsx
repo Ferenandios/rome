cfbe198b787bc7920a55aec3971d0673643a59ec
Ferenandios <www.terunplay@gmail.com>
2024-04-10 14:03:55 +0300

    Created Button-component

---
+++ src/components/Message/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import { toggleIsShowContact } from "../../../features/global.slice";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
+
+const Button: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { colorTheme } = useAppSelector((state) => state.global);
+  const { logoIcon } = useAppSelector((state) => state.message);
+  const handleClick = () => {
+    dispatch(toggleIsShowContact());
+  };
+  return (
+    <>
+      <button
+        onMouseUp={handleClick}
+        style={{ backgroundColor: colorTheme }}
+        className={`flex justify-center items-center 
+          w-[50px] h-[50px] rounded-full`}
+      >
+        <img className="w-[47%]" src={logoIcon} alt="Write" />
+      </button>
+    </>
+  );
+};
+
+export default Button;
