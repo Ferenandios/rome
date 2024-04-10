b12e0e5232dadd737c98ff42a27f2f657d5daddb
Ferenandios <www.terunplay@gmail.com>
2024-04-10 14:03:11 +0300

    Added Button & Animation components

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,24 +1,14 @@
 import { FC } from "react";
-import { useAppDispatch, useAppSelector } from "../../hooks";
-import { toggleIsShowContact } from "../../features/global.slice";
+import Animation from "./Animation/Animation";
+import Button from "./Button/Button";
 
 const Message: FC = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const { colorTheme } = useAppSelector((state) => state.global);
-  const { logoIcon } = useAppSelector((state) => state.message);
-  const handleClick = () => {
-    dispatch(toggleIsShowContact());
-  };
   return (
     <>
-      <button
-        onMouseUp={handleClick}
-        style={{ backgroundColor: colorTheme }}
-        className={`animate-bounce md:hidden fixed z-30 bottom-[15px] right-[15px] flex justify-center items-center 
-          w-[50px] h-[50px] rounded-full`}
-      >
-        <img className="w-[47%]" src={logoIcon} alt="Write" />
-      </button>
+      <div className="md:hidden animate-bounce fixed z-30 bottom-[15px] right-[15px]">
+        <Animation />
+        <Button />
+      </div>
     </>
   );
 };
