e3e8931f8d77790cc7c743708ab27e056f7c37b2
Ferenandios <www.terunplay@gmail.com>
2024-04-10 14:03:44 +0300

    Created Animation-component

---
+++ src/components/Message/Animation/Animation.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { useAppSelector } from "../../../hooks";
+
+// position: absolute;
+//     top: 0;
+//     left: 0;
+//     bottom: 0;
+//     right: 0;
+//     border: 1px solid #00aeef;
+//     border-radius: 50%;
+
+const Animation: FC = (): JSX.Element => {
+  const { colorTheme } = useAppSelector((state) => state.global);
+  return (
+    <>
+      <div
+        style={{ borderColor: colorTheme }}
+        className="animate-widgetPulse absolute top-0 left-0 bottom-0 right-0 border-[1px] rounded-[50%] cursor-pointer"
+      ></div>
+    </>
+  );
+};
+
+export default Animation;
