41e3c52ca59051c4550f598c70a7c00fec6592ff
Ferenandios <www.terunplay@gmail.com>
2024-04-11 08:43:41 +0300

    Deleted useless-pomoika animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,6 @@ export default {
   theme: {
     fontFamily: { sans: ["Open Sans", "sans-serif"] },
   },
-  extend: {
-    animation: {
-      widgetPulse: "widgetPulse infinite 1.5",
-    },
-    keyframes: {
-      widgetPulse: {
-        "0% 50%": {
-          transform: "scale(1, 1)",
-          opacity: 1,
-        },
-        "100%": {
-          transform: "scale(2, 2)",
-          opacity: 0,
-        },
-      },
-    },
-  },
+  extend: {},
   plugins: [],
 };
