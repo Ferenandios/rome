128b59c1082b358dc227d1decf8883de5bcf3357
Ferenandios <www.terunplay@gmail.com>
2024-04-11 08:44:35 +0300

    Added pulse animation & Added opacity background

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,13 +1,31 @@
 import { FC } from "react";
 import Animation from "./Animation/Animation";
 import Button from "./Button/Button";
+import { useAppSelector } from "../../hooks";
+
+// position: absolute;
+//     top: -8px;
+//     left: -8px;
+//     height: 82px;
+//     min-width: 66px;
+//     -webkit-width: calc(100% + 16px);
+//     width: calc(100% + 16px);
+//     border-radius: 100px;
+//     background: #00aeef;
+//     opacity: .2;
 
 const Message: FC = (): JSX.Element => {
+  const { colorTheme } = useAppSelector((state) => state.global);
   return (
     <>
-      <div className="md:hidden animate-bounce fixed z-30 bottom-[15px] right-[15px]">
-        <Animation />
-        <Button />
+      <div
+        style={{ background: colorTheme + "33" }}
+        className="md:hidden fixed z-30 bottom-[35px] right-[35px] flex justify-center items-center bg-opacity-20 w-[69px] h-[69px] rounded-full"
+      >
+        <div className="relative w-[56px] h-[56px]">
+          <Animation />
+          <Button />
+        </div>
       </div>
     </>
   );
