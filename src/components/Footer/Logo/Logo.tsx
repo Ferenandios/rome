ca56a8cf87d98db2b57c252749da93ed7eb93c03
Ferenandios <www.terunplay@gmail.com>
2024-04-11 10:06:29 +0300

    Changed useSelector

--- src/components/Footer/Logo/Logo.tsx
+++ src/components/Footer/Logo/Logo.tsx
@@ -2,11 +2,11 @@ import { FC } from "react";
 import { useAppSelector } from "../../../hooks";
 
 const Logo: FC = (): JSX.Element => {
-  const { logoIcon } = useAppSelector((state) => state.footer);
+  const { companyLogo } = useAppSelector((state) => state.global);
   return (
     <>
       <div className="flex justify-center w-full h-[50px] mt-[24px]">
-        <img className="h-full" src={logoIcon} alt="Logo" />
+        <img className="h-full" src={companyLogo} alt="Logo" />
       </div>
     </>
   );
