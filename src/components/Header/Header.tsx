f536de0689e432d4200fd49ae5bea385b85b9409
Ferenandios <www.terunplay@gmail.com>
2024-04-11 13:35:15 +0300

    Added Responsive Design & Added Contact-component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,8 @@ import { FC } from "react";
 import Burger from "./Burger/Burger";
 import Search from "./Search/Search";
 import Logo from "./Logo/Logo";
-import Button from "./Button/Button";
+import Button from "./Contact/Button/Button";
+import Contact from "./Contact/Contact";
 
 const Header: FC = (): JSX.Element => {
   return (
@@ -10,12 +11,12 @@ const Header: FC = (): JSX.Element => {
       <header>
         <div className="container px-[8px] mx-auto">
           <div className="flex justify-between h-[50px] lg:h-[100px]">
-            <div className="flex w-full gap-[8px]">
+            <div className="flex w-full gap-[8px] lg:gap-[20px]">
               <Logo />
               <Burger />
               <Search />
             </div>
-            <Button />
+            <Contact />
           </div>
         </div>
       </header>
