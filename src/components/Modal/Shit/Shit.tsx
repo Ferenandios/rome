795dbae7f66595bff161218479a07afeb54eb5c7
Ferenandios <www.terunplay@gmail.com>
2024-04-11 16:02:00 +0300

    Created Shit-component in Modal-component

---
+++ src/components/Modal/Shit/Shit.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Shit: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className="absolute w-full h-full bg-black bg-opacity-40"></div>
+    </>
+  );
+};
+
+export default Shit;
