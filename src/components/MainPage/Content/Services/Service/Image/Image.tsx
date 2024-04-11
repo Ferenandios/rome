f985776101819fd6e18da4b3ca6f60ce2735f844
Ferenandios <www.terunplay@gmail.com>
2024-04-11 17:32:53 +0300

    Added background-color for image

--- src/components/MainPage/Content/Services/Service/Image/Image.tsx
+++ src/components/MainPage/Content/Services/Service/Image/Image.tsx
@@ -5,7 +5,7 @@ const Image: FC<{ serivceId: number }> = ({ serivceId }): JSX.Element => {
   const { image } = useAppSelector((state) => state.global.services[serivceId]);
   return (
     <>
-      <div className="w-full h-[200px] sm:h-[400px] md:h-[200px]">
+      <div className="w-full h-[200px] bg-black bg-opacity-10 sm:h-[400px] md:h-[200px]">
         <img
           className="w-full h-full sm:object-cover"
           src={image}
