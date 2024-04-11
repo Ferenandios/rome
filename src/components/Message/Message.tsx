c20f3e4a7559ecd3dcfac01b4c8feb1995753504
Ferenandios <www.terunplay@gmail.com>
2024-04-11 09:14:16 +0300

    Added isShowMessage property & Added Timeout for change in useEffect

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,32 +1,30 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { toggleIsShowMessage } from "../../features/global.slice";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import Animation from "./Animation/Animation";
 import Button from "./Button/Button";
-import { useAppSelector } from "../../hooks";
-
-// position: absolute;
-//     top: -8px;
-//     left: -8px;
-//     height: 82px;
-//     min-width: 66px;
-//     -webkit-width: calc(100% + 16px);
-//     width: calc(100% + 16px);
-//     border-radius: 100px;
-//     background: #00aeef;
-//     opacity: .2;
 
 const Message: FC = (): JSX.Element => {
-  const { colorTheme } = useAppSelector((state) => state.global);
+  const dispatch = useAppDispatch();
+  const { colorTheme, isShowMessage } = useAppSelector((state) => state.global);
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(toggleIsShowMessage());
+    }, 3000);
+  }, []);
   return (
     <>
-      <div
-        style={{ background: colorTheme + "33" }}
-        className="md:hidden fixed z-30 bottom-[35px] right-[35px] flex justify-center items-center bg-opacity-20 w-[69px] h-[69px] rounded-full"
-      >
-        <div className="relative w-[56px] h-[56px]">
-          <Animation />
-          <Button />
+      {isShowMessage && (
+        <div
+          style={{ background: colorTheme + "33" }}
+          className="md:hidden animate-widgetVisible fixed z-30 bottom-[35px] right-[35px] flex justify-center items-center bg-opacity-20 w-[69px] h-[69px] rounded-full"
+        >
+          <div className="relative w-[56px] h-[56px]">
+            <Animation />
+            <Button />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
