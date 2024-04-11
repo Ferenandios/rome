4a3e32e9f1291c3ea62b3c0d3f8e3bd25ab72d34
Ferenandios <www.terunplay@gmail.com>
2024-04-11 12:47:21 +0300

    Created Locate-component

---
+++ src/components/Footer/Locate/Locate.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Copyright from "./Copyright/Copyright";
+import Address from "./Address/Address";
+
+const Locate: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className="flex flex-col items-end gap-[8px]">
+        <Copyright />
+        <Address />
+      </div>
+    </>
+  );
+};
+
+export default Locate;
