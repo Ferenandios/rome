5634494b138d3e3761ecea548d9f3a0452a6a9b0
Ferenandios <www.terunplay@gmail.com>
2024-04-11 18:08:36 +0300

    Added exitIcom property in State

--- src/features/modal.slice.ts
+++ src/features/modal.slice.ts
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { IState } from "../types/Modal/types";
 
+import exitIconAsset from "/src/assets/Modal/Exit.svg";
+
 const initialState: IState = {
   title: "Обратная связь",
+  exitIcon: exitIconAsset,
 };
 
 const modalSlice = createSlice({ name: "modal", initialState, reducers: {} });
