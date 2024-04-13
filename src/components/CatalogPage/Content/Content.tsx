d5960548d8ec664326c78fc3d48e837289d0bb5e
Ferenandios <www.terunplay@gmail.com>
2024-04-13 18:37:35 +0300

    Created Inner & Bottom-component

--- src/components/CatalogPage/Content/Content.tsx
+++ src/components/CatalogPage/Content/Content.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
-import Image from "./Image/Image";
+import Top from "./Top/Top";
+import Line from "./Line/Line";
+import Bottom from "./Bottom/Bottom";
 
 const Content: FC = (): JSX.Element => {
   return (
     <>
       <section>
         <div className="container p-[16px] mx-auto">
-          <Image />
+          <div className="flex flex-col gap-[8px]">
+            <Top />
+            <Line />
+            <Bottom />
+          </div>
         </div>
       </section>
     </>
