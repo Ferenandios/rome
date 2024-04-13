1ad594de0b3dda48b28f2927453d543e1fe7a3cf
Ferenandios <www.terunplay@gmail.com>
2024-04-14 01:48:39 +0300

    Added Form-component

--- src/components/CatalogPage/Content/Bottom/Bottom.tsx
+++ src/components/CatalogPage/Content/Bottom/Bottom.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import Description from "./Description/Description";
+import Form from "../../../Form/Form";
 
 const Bottom: FC = (): JSX.Element => {
   return (
     <>
       <div>
         <Description />
+        <Form />
       </div>
     </>
   );
