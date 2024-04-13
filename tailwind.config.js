3a9b097b31a449f0b5c2343b6bacf1b335858af5
Ferenandios <www.terunplay@gmail.com>
2024-04-14 01:15:06 +0300

    Added inter the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: { sans: ["Open Sans", "sans-serif"] },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
   },
   extend: {},
   plugins: [],
