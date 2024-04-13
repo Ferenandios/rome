a09847174f40262e9065016c3ca7bf93647073b0
Ferenandios <www.terunplay@gmail.com>
2024-04-13 14:07:51 +0300

    Created Pages for items of Catalog

--- src/components/CatalogPage/CatalogPage.tsx
+++ src/components/CatalogPage/CatalogPage.tsx
@@ -1,12 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { setCurrentService } from "../../features/global.slice";
 
 const CatalogPage: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { itemId } = useParams();
+  const { services } = useAppSelector((state) => state.global);
+  const service =
+    itemId !== undefined && !isNaN(+itemId) ? services[+itemId] : null;
+  useEffect(() => {
+    service && dispatch(setCurrentService(service));
+  });
   return (
     <>
       <Header />
-      <>Catalog</>
+      {service ? service.title : "rolfanPomoika"}
       <Footer />
     </>
   );
