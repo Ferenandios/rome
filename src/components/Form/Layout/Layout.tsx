413c0265fcc3ee6cd8732fc44920cfbfc9ef8a12
Ferenandios <www.terunplay@gmail.com>
2024-04-14 13:38:55 +0300

    Created Text-component in Form-component

---
+++ src/components/Form/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { useAppSelector } from "../../../hooks";
+import Text from "./Text/Text";
+
+const Layout: FC = (): JSX.Element => {
+  const { layout } = useAppSelector((state) => state.global.currentService);
+  return (
+    <>
+      <ol className="flex flex-col gap-[8px] list-decimal pt-[8px] px-[16px]">
+        {layout.map((text, index) => (
+          <Text key={index} text={text} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default Layout;
