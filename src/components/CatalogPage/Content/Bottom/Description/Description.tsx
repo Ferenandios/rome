68bc1fdf1ced2545b8484840c2f05b90b2332ac8
Ferenandios <www.terunplay@gmail.com>
2024-04-14 20:40:09 +0300

    Added title if describtion is empty

--- src/components/CatalogPage/Content/Bottom/Description/Description.tsx
+++ src/components/CatalogPage/Content/Bottom/Description/Description.tsx
@@ -10,33 +10,43 @@ const Description: FC = (): JSX.Element => {
   );
   return (
     <>
-      <div className="flex flex-col gap-[12px]">
-        {description.map((block, index) => (
-          <Fragment key={index}>
-            {block.type === "title" && (
-              <>
-                {block.text.map((text, i) => (
-                  <Title key={i} text={text} />
-                ))}
-              </>
-            )}
-            {block.type === "ul" && (
-              <ul className="flex flex-col list-disc gap-[8px] pl-[20px]">
-                {block.text.map((text, i) => (
-                  <ListItem key={i} text={text} />
-                ))}
-              </ul>
-            )}
-            {block.type === "p" && (
-              <>
-                {block.text.map((text, i) => (
-                  <Paragraph key={i} text={text} />
-                ))}
-              </>
-            )}
-          </Fragment>
-        ))}
-      </div>
+      {description.length ? (
+        <>
+          <div className="flex flex-col gap-[12px]">
+            {description.map((block, index) => (
+              <Fragment key={index}>
+                {block.type === "title" && (
+                  <>
+                    {block.text.map((text, i) => (
+                      <Title key={i} text={text} />
+                    ))}
+                  </>
+                )}
+                {block.type === "ul" && (
+                  <ul className="flex flex-col list-disc gap-[8px] pl-[20px]">
+                    {block.text.map((text, i) => (
+                      <ListItem key={i} text={text} />
+                    ))}
+                  </ul>
+                )}
+                {block.type === "p" && (
+                  <>
+                    {block.text.map((text, i) => (
+                      <Paragraph key={i} text={text} />
+                    ))}
+                  </>
+                )}
+              </Fragment>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="font-inter text-[16px] opacity-60">
+            Описание отсутствует.
+          </h1>
+        </>
+      )}
     </>
   );
 };
