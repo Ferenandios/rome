169a31d8bf5c64e3cfeb1ce457c367eb1d55bf6f
Ferenandios <www.terunplay@gmail.com>
2024-04-14 13:43:07 +0300

    Added gap style for Inner

--- src/components/CatalogPage/Content/Bottom/Bottom.tsx
+++ src/components/CatalogPage/Content/Bottom/Bottom.tsx
@@ -5,7 +5,7 @@ import Form from "../../../Form/Form";
 const Bottom: FC = (): JSX.Element => {
   return (
     <>
-      <div>
+      <div className="flex flex-col gap-[16px]">
         <Description />
         <Form />
       </div>
