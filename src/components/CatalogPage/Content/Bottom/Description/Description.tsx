b4be162ce42152181a2b16a2c1bac74408681a05
Ferenandios <www.terunplay@gmail.com>
2024-04-14 13:42:45 +0300

    Fixed key value

--- src/components/CatalogPage/Content/Bottom/Description/Description.tsx
+++ src/components/CatalogPage/Content/Bottom/Description/Description.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import { useAppSelector } from "../../../../../hooks";
-import Title from "./Title/Title";
 import ListItem from "./ListItem/ListItem";
 import Paragraph from "./Paragraph/Paragraph";
+import Title from "./Title/Title";
 
 const Description: FC = (): JSX.Element => {
   const { description } = useAppSelector(
@@ -11,30 +11,30 @@ const Description: FC = (): JSX.Element => {
   return (
     <>
       <div className="flex flex-col gap-[12px]">
-        {description.map((block) => (
-          <>
+        {description.map((block, index) => (
+          <Fragment key={index}>
             {block.type === "title" && (
               <>
-                {block.text.map((text) => (
-                  <Title key={block.id} text={text} />
+                {block.text.map((text, i) => (
+                  <Title key={i} text={text} />
                 ))}
               </>
             )}
             {block.type === "ul" && (
               <ul className="flex flex-col list-disc gap-[8px] pl-[20px]">
-                {block.text.map((text) => (
-                  <ListItem key={block.id} text={text} />
+                {block.text.map((text, i) => (
+                  <ListItem key={i} text={text} />
                 ))}
               </ul>
             )}
             {block.type === "p" && (
               <>
-                {block.text.map((text) => (
-                  <Paragraph key={block.id} text={text} />
+                {block.text.map((text, i) => (
+                  <Paragraph key={i} text={text} />
                 ))}
               </>
             )}
-          </>
+          </Fragment>
         ))}
       </div>
     </>
