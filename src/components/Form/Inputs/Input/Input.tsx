7d76ed7b0b1f7f181c9f00925305b90d6e651ca2
Ferenandios <www.terunplay@gmail.com>
2024-04-14 13:39:34 +0300

    Created Input-component in Form-component

---
+++ src/components/Form/Inputs/Input/Input.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { useAppSelector } from "../../../../hooks";
+import { IInput } from "../../../../types/Form/types";
+
+const Input: FC<{ input: IInput }> = ({ input }): JSX.Element => {
+  const { colorTheme } = useAppSelector((state) => state.global);
+  return (
+    <>
+      <div className="flex flex-col">
+        <p className="font-bold">{input.title}</p>
+        <input
+          style={{ borderColor: colorTheme }}
+          className="font-bold w-full h-[36px] border-[2px] rounded-[7px] pl-[4px]"
+          placeholder={input.placeholder}
+          type="text"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Input;
