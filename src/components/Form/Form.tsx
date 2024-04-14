1bc7f2b2a3056590287e7aadffaa12bdbe1335f2
Ferenandios <www.terunplay@gmail.com>
2024-04-14 20:39:43 +0300

    Added styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,13 +3,20 @@ import Inputs from "./Inputs/Inputs";
 import Textareas from "./Textareas/Textareas";
 import Button from "./Button/Button";
 import Layout from "./Layout/Layout";
+import { useAppSelector } from "../../hooks";
 
 const Form: FC<{ width?: string }> = ({ width }): JSX.Element => {
+  const { layout } = useAppSelector((state) => state.global.currentService);
   return (
     <>
-      <div className="md:flex md:justify-between">
+      <div
+        style={{ justifyContent: !layout.length ? "center" : "space-between" }}
+        className="md:flex"
+      >
         <div
-          style={{ width: width !== undefined ? width : "" }}
+          style={{
+            width: width !== undefined ? width : "",
+          }}
           className="flex flex-col gap-[8px] md:w-[440px]"
         >
           <Inputs />
