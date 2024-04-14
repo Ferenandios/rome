095141198d6264ddaa4c193a7640645eb931ceef
Ferenandios <www.terunplay@gmail.com>
2024-04-14 20:25:59 +0300

    Added display component-by-length

--- src/components/Form/Layout/Layout.tsx
+++ src/components/Form/Layout/Layout.tsx
@@ -6,11 +6,15 @@ const Layout: FC = (): JSX.Element => {
   const { layout } = useAppSelector((state) => state.global.currentService);
   return (
     <>
-      <ol className="flex flex-col gap-[8px] list-decimal pt-[8px] px-[16px] md:w-[280px] md:pt-[20px]">
-        {layout.map((text, index) => (
-          <Text key={index} text={text} />
-        ))}
-      </ol>
+      {layout.length > 0 && (
+        <>
+          <ol className="flex flex-col gap-[8px] list-decimal pt-[8px] px-[16px] md:w-[280px] md:pt-[20px]">
+            {layout.map((text, index) => (
+              <Text key={index} text={text} />
+            ))}
+          </ol>
+        </>
+      )}
     </>
   );
 };
