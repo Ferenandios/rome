18f841f0295d573a7f4aa687740f85c514e530ba
Ferenandios <www.terunplay@gmail.com>
2024-04-15 18:22:08 +0300

    Changed divs and component to Service-component

--- src/components/CatalogPage/Content/Content.tsx
+++ src/components/CatalogPage/Content/Content.tsx
@@ -1,18 +1,12 @@
 import { FC } from "react";
-import Top from "./Top/Top";
-import Line from "./Line/Line";
-import Bottom from "./Bottom/Bottom";
+import Service from "./Service/Service";
 
 const Content: FC = (): JSX.Element => {
   return (
     <>
       <section>
         <div className="container p-[16px] mx-auto">
-          <div className="flex flex-col gap-[8px]">
-            <Top />
-            <Line />
-            <Bottom />
-          </div>
+          <Service />
         </div>
       </section>
     </>
