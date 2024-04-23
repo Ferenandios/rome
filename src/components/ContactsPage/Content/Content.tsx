2cc2793dd20b8c777e2b04b184d3f001fb051f9e
Ferenandios <www.terunplay@gmail.com>
2024-04-23 20:20:10 +0300

    Created Content-component in ContactsPage-component

---
+++ src/components/ContactsPage/Content/Content.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Content: FC = (): JSX.Element => {
+  return (
+    <>
+      <section>
+        <h1>lmao xd</h1>
+      </section>
+    </>
+  );
+};
+
+export default Content;
