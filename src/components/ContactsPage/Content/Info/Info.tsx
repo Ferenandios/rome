8bf83a1feb80eba33fd58cad92f9c677b868c2ea
Ferenandios <www.terunplay@gmail.com>
2024-04-24 16:28:38 +0300

    Added Contacts-component

--- src/components/ContactsPage/Content/Info/Info.tsx
+++ src/components/ContactsPage/Content/Info/Info.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import Address from "./Address/Address";
+import Contacts from "./Contacts/Contacts";
+import styles from "./Info.module.css";
 
 const Info: FC = (): JSX.Element => {
   return (
     <>
-      <div>
+      <div className={styles.inner}>
         <Address />
+        <Contacts />
       </div>
     </>
   );
