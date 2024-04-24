6dedbd5e554c939c6abf1401d6b47334ce43e6ac
Ferenandios <www.terunplay@gmail.com>
2024-04-24 16:29:44 +0300

    Added Map & Form components

--- src/components/ContactsPage/Content/Content.tsx
+++ src/components/ContactsPage/Content/Content.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
 import Info from "./Info/Info";
+import Form from "../../Form/Form";
+import Map from "./Map/Map";
+import styles from "./Content.module.css";
 
 const Content: FC = (): JSX.Element => {
   return (
     <>
-      <section className="min-h-[500px]">
-        <Info />
+      <section className={styles.section}>
+        <div className={styles.containerr}>
+          <div className={styles.inner}>
+            <Info />
+            <Map />
+            <Form layout="hidden" />
+          </div>
+        </div>
       </section>
     </>
   );
