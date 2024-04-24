3a23a2e082a21bbb81b34aa79e99ba6414f8e1d0
Ferenandios <www.terunplay@gmail.com>
2024-04-24 15:35:27 +0300

    Created Info-component in ContactsPage-component

---
+++ src/components/ContactsPage/Content/Info/Info.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import Address from "./Address/Address";
+
+const Info: FC = (): JSX.Element => {
+  return (
+    <>
+      <div>
+        <Address />
+      </div>
+    </>
+  );
+};
+
+export default Info;
