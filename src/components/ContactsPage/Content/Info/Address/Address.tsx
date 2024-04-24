2341fc2450cc0f00302ed59d13170930106998a2
Ferenandios <www.terunplay@gmail.com>
2024-04-24 16:29:07 +0300

    Added imported styles

--- src/components/ContactsPage/Content/Info/Address/Address.tsx
+++ src/components/ContactsPage/Content/Info/Address/Address.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { useAppSelector } from "../../../../../hooks";
+import styles from "./Address.module.css";
 
 const Address: FC = (): JSX.Element => {
   const { companyAddress } = useAppSelector((state) => state.global);
   return (
     <>
-      <div>
-        <span className="text-[12px]">{companyAddress}</span>
+      <div className={styles.ansoc}>
+        <span className={styles.text}>{companyAddress}</span>
       </div>
     </>
   );
