63055ef48aedf0c6c05f37b29e633159a8a1e74a
Ferenandios <www.terunplay@gmail.com>
2024-04-25 18:51:17 +0300

    Added show-component-by-isShowModal property

--- src/components/Modal/Shit/Shit.tsx
+++ src/components/Modal/Shit/Shit.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
+import { useAppSelector } from "../../../hooks";
 
 const Shit: FC = (): JSX.Element => {
+  const { isShowModal } = useAppSelector((state) => state.global);
   return (
     <>
-      <div className="absolute w-full h-full bg-black bg-opacity-40"></div>
+      {isShowModal && (
+        <div className="absolute w-full h-full bg-black bg-opacity-40"></div>
+      )}
     </>
   );
 };
