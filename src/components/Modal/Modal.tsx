de416a161e2a5475a4644da5cc20f0ea4b03222e
Ferenandios <www.terunplay@gmail.com>
2024-04-25 18:51:57 +0300

    Added Content-component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,26 +1,19 @@
 import { FC } from "react";
-import Shit from "./Shit/Shit";
-import Title from "./Title/Title";
-import Exit from "./Exit/Exit";
 import { useAppSelector } from "../../hooks";
+import Content from "./Content/Content";
+import Shit from "./Shit/Shit";
 
 const Modal: FC = (): JSX.Element => {
   const { isShowModal } = useAppSelector((state) => state.global);
   return (
     <>
+      <Shit />
       <div
-        className={`absolute z-50 top-0 ${
-          isShowModal ? "w-screen" : "w-0"
-        } h-screen overflow-hidden origin-right duration-500`}
+        style={{ width: isShowModal ? "100vw" : "0vw" }}
+        className="absolute z-50 top-0 flex justify-end w-screen h-screen duration-500"
       >
         <div className="relative w-full h-full">
-          <Shit />
-          <div className={`absolute right-0 w-full h-full bg-white p-[16px]`}>
-            <div className="relative">
-              <Exit />
-              <Title />
-            </div>
-          </div>
+          <Content />
         </div>
       </div>
     </>
