af083b2eedfbe30e28fb7d831f8579a4ba7332cd
Ferenandios <www.terunplay@gmail.com>
2024-04-26 10:30:09 +0300

    Moved ansoc in Content-component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,12 @@
 import { FC } from "react";
 import Content from "./Content/Content";
 import Shit from "./Shit/Shit";
-import { useAppSelector } from "../../hooks";
 
 const Modal: FC = (): JSX.Element => {
-  const { isShowModal } = useAppSelector((state) => state.global);
   return (
     <>
       <Shit />
-      <div
-        style={{ width: isShowModal ? "500px" : "0" }}
-        className="absolute right-0 z-50 top-0 flex justify-end w-0 h-screen duration-500 overflow-hidden"
-      >
-        <Content />
-      </div>
+      <Content />
     </>
   );
 };
