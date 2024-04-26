7c97ddb55de2d2f2bc61cf6cdecf7f5cf929e7b7
Ferenandios <www.terunplay@gmail.com>
2024-04-26 10:33:37 +0300

    Added styles for Inner & Ansoc

--- src/components/Modal/Content/Content.tsx
+++ src/components/Modal/Content/Content.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
 import Inner from "./Inner/Inner";
 import Top from "./Top/Top";
+import { useAppSelector } from "../../../hooks";
 
 const Content: FC = (): JSX.Element => {
+  const { isShowModal, colorTheme } = useAppSelector((state) => state.global);
   return (
     <>
-      <div className="absolute w-[500px] h-screen bg-lime-200 p-[16px]">
-        <Top />
-        <Inner />
+      <div
+        style={{ width: isShowModal ? "500px" : "0" }}
+        className="absolute right-0 z-50 top-0 flex justify-end w-0 h-screen duration-500 overflow-hidden"
+      >
+        <div
+          style={{ borderColor: colorTheme }}
+          className="absolute flex flex-col w-[500px] h-screen bg-white border-b-[35px] gap-[8px] p-[16px]"
+        >
+          <Top />
+          <Inner />
+        </div>
       </div>
     </>
   );
