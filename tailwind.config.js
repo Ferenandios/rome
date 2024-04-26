9dcfb4a6668938b5da4d2801f54c2bfe8c9654ac
Ferenandios <www.terunplay@gmail.com>
2024-04-26 17:28:16 +0300

    Added screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      hsm: { raw: "(max-height: 480px)" },
+    },
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
       inter: ["Inter", "sans-serif"],
     },
   },
-  extend: {
-    screens: {
-      tall: { raw: "(min-height: 800px)" },
-      // => @media (min-height: 800px) { ... }
-    },
-  },
+  extend: {},
   plugins: [],
 };
