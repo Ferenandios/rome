77bc16e14278f2a947e4e497bc1c28dde17d915a
Ferenandios <www.terunplay@gmail.com>
2024-04-26 20:54:54 +0300

    Added more-readable hierarchy of children components

--- src/components/Footer/Info/Info.tsx
+++ src/components/Footer/Info/Info.tsx
@@ -1,64 +1,16 @@
-import { FC, Fragment } from "react";
-import { useAppSelector } from "../../../hooks";
-import { type titleType } from "../../../types/Footer/types";
-import Links from "./Links/Links";
-import Title from "./Title/Title";
+import { FC } from "react";
+import Categories from "./Categories/Categories";
 import styles from "./Info.module.css";
 
 const Info: FC = (): JSX.Element => {
-  const { categories, contacts } = useAppSelector((state) => state.footer);
-  const titles: titleType[] = ["Категории", "Контакты"];
   return (
     <>
       <div className={styles.inner}>
-        {titles.map((title, index) => (
-          <Fragment key={index}>
-            {/* NONE */}
-            <div
-              className={`md:hidden ${
-                title === "Контакты" && "text-right"
-              } flex flex-col gap-[4px]`}
-            >
-              <Title title={title} />
-              <div>
-                {title === "Категории" && (
-                  <>
-                    {categories.map((category) => (
-                      <Links key={category.id} element={category} />
-                    ))}
-                  </>
-                )}
-                {title === "Контакты" && (
-                  <>
-                    {contacts.map((contact) => (
-                      <Links key={contact.id} element={contact} />
-                    ))}
-                  </>
-                )}
-              </div>
-            </div>
-            {/* MD */}
-            <div className="hidden md:flex flex-col gap-[4px]" key={index}>
-              <Title title={title} />
-              <div>
-                {title === "Категории" && (
-                  <>
-                    {categories.map((category) => (
-                      <Links key={category.id} element={category} />
-                    ))}
-                  </>
-                )}
-                {title === "Контакты" && (
-                  <>
-                    {contacts.map((contact) => (
-                      <Links key={contact.id} element={contact} />
-                    ))}
-                  </>
-                )}
-              </div>
-            </div>
-          </Fragment>
-        ))}
+        <Categories />
+        {/* Create Contacts component like
+        upper Categories-component
+        BLESSRNG do not be a down
+        please */}
       </div>
     </>
   );
