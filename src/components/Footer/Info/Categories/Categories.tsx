ae8919505371b41c2bba388386a035afbc32366e
Ferenandios <www.terunplay@gmail.com>
2024-04-27 06:37:08 +0300

    Added imported styles

--- src/components/Footer/Info/Categories/Categories.tsx
+++ src/components/Footer/Info/Categories/Categories.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import Crumbs from "./Crumbs/Crumbs";
 import Title from "./Title/Title";
+import styles from './Categories.module.css'
 
 const Categories: FC = (): JSX.Element => {
   return (
     <>
-      <div className="flex flex-col gap-[4px]">
+      <div className={styles.inner}>
         <Title />
         <Crumbs />
       </div>
