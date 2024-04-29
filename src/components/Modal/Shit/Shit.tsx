1375ec006f3c4c51fb2df9bb23dbc84182f51e79
Ferenandios <www.terunplay@gmail.com>
2024-04-29 14:21:46 +0300

    Added handleClick for a Shit

--- src/components/Modal/Shit/Shit.tsx
+++ src/components/Modal/Shit/Shit.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
-import { useAppSelector } from "../../../hooks";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
 import styles from "./Shit.module.css";
+import { toggleIsShowModal } from "../../../features/global.slice";
 
 const Shit: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
   const { isShowModal } = useAppSelector((state) => state.global);
-  return <>{isShowModal && <div className={styles.shit}></div>}</>;
+  const handleClick = () => {
+    dispatch(toggleIsShowModal());
+  };
+  return (
+    <>
+      {isShowModal && <div onClick={handleClick} className={styles.shit}></div>}
+    </>
+  );
 };
 
 export default Shit;
