3cfa9c8d2543286c06df22e96d1cb17fa4b3677a
Ferenandios <www.terunplay@gmail.com>
2024-04-29 13:16:12 +0300

    Added Responsive styles for a Form-component

--- src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
+++ src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
@@ -2,13 +2,19 @@ import { FC } from "react";
 import Description from "./Description/Description";
 import Form from "../../../../Form/Form";
 import styles from "./Bottom.module.css";
+import { useAppSelector } from "../../../../../hooks";
 
 const Bottom: FC = (): JSX.Element => {
+  const { description } = useAppSelector(
+    (state) => state.global.currentService
+  );
   return (
     <>
       <div className={styles.inner}>
         <Description />
-        <Form />
+        <div className="flex lg:hidden">
+          {description.length === 0 && <Form />}
+        </div>
       </div>
     </>
   );
