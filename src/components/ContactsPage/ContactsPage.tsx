35c14a66c401250b78f105b7c508f59377824a6d
Ferenandios <www.terunplay@gmail.com>
2024-04-29 13:08:43 +0300

    Added main-tag as a parent

--- src/components/ContactsPage/ContactsPage.tsx
+++ src/components/ContactsPage/ContactsPage.tsx
@@ -5,17 +5,25 @@ import Header from "../Header/Header";
 import Message from "../Message/Message";
 import Modal from "../Modal/Modal";
 import Content from "./Content/Content";
+import { useAppSelector } from "../../hooks";
 
 const ContactsPage: FC = (): JSX.Element => {
+  const { burgerIsOpen, isShowModal } = useAppSelector((state) => state.global);
   return (
     <>
-      <Header />
-      <Crumbs />
-      <Content />
-      <Footer />
+      <main
+        className={`${
+          burgerIsOpen || isShowModal ? "w-screen h-screen overflow-hidden" : ""
+        }`}
+      >
+        <Header />
+        <Crumbs />
+        <Content />
+        <Footer />
 
-      <Message />
-      <Modal />
+        <Message />
+        <Modal />
+      </main>
     </>
   );
 };
