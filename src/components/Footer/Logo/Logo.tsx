ca791920daa7b04056863a485ffd92714c14d0d4
Ferenandios <www.terunplay@gmail.com>
2024-04-29 13:07:52 +0300

    Added Link

--- src/components/Footer/Logo/Logo.tsx
+++ src/components/Footer/Logo/Logo.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import { useAppSelector } from "../../../hooks";
 import styles from "./Logo.module.css";
+import { Link } from "react-router-dom";
 
 const Logo: FC = (): JSX.Element => {
   const { companyLogo } = useAppSelector((state) => state.global);
   return (
     <>
       <div className={styles.ansoc}>
-        <img className={styles.img} src={companyLogo} alt="Logo" />
+        <Link to={"/rome"}>
+          <img className={styles.img} src={companyLogo} alt="Logo" />
+        </Link>
       </div>
     </>
   );
