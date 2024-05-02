a8c41524fb87629c6e9cd06d1666bee87238552c
Ferenandios <www.terunplay@gmail.com>
2024-05-02 15:35:53 +0300

    Initial commit

--- src/components/Modal/Content/Inner/Send/Send.tsx
+++ src/components/Modal/Content/Inner/Send/Send.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import Layout from "./Layout/Layout";
 import Form from "../../../../Form/Form";
-import styles from './Send.module.css'
+import styles from "./Send.module.css";
 
 const Send: FC = (): JSX.Element => {
   return (
