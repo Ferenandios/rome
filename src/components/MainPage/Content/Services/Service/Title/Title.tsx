12dee06fed679da684185a1cae10dc3149857d65
Ferenandios <www.terunplay@gmail.com>
2024-05-03 06:33:03 +0300

    Added imported styles

--- src/components/MainPage/Content/Services/Service/Title/Title.tsx
+++ src/components/MainPage/Content/Services/Service/Title/Title.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 import { useAppSelector } from "../../../../../../hooks";
+import styles from "./Title.module.css";
 
 const Title: FC<{ serviceId: number }> = ({ serviceId }): JSX.Element => {
   const { title } = useAppSelector((state) => state.global.services[serviceId]);
   return (
     <>
-      <div className="flex justify-center items-center w-full h-[60px] bg-white overflow-hidden sm:h-[75px] md:h-[60px]">
-        <h3 className="font-bold text-[14px] text-center max-h-full leading-[16px] sm:text-[18px] md:text-[14px]">
-          {title}
-        </h3>
+      <div className={styles.ansoc}>
+        <h3 className={styles.title}>{title}</h3>
       </div>
     </>
   );
