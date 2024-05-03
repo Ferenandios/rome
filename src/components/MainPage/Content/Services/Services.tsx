b1a186b70dd03cd9cd45f89b1ea3aff518557b98
Ferenandios <www.terunplay@gmail.com>
2024-05-03 06:32:56 +0300

    Added imported styles

--- src/components/MainPage/Content/Services/Services.tsx
+++ src/components/MainPage/Content/Services/Services.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { useAppSelector } from "../../../../hooks";
 import Service from "./Service/Service";
+import styles from './Services.module.css'
 
 const Services: FC = (): JSX.Element => {
   const { services } = useAppSelector((state) => state.global);
   const { emptyServicesTitle } = useAppSelector((state) => state.mainContent);
   return (
     <>
-      <div className="flex flex-col items-center gap-[12px] md:flex-row md:flex-wrap md:justify-center lg:max-w-[1000px] lg:gap-[27px]">
+      <div className={styles.inner}>
         {services.length ? (
           <>
             {services.map((service) => (
@@ -16,7 +17,7 @@ const Services: FC = (): JSX.Element => {
           </>
         ) : (
           <>
-            <h3 className="text-[16px] opacity-60 h-[450px]">
+            <h3 className={styles.title}>
               {emptyServicesTitle}
             </h3>
           </>
