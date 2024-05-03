a9cc887e6f07cd80900a73c5f4159e256f0daa46
Ferenandios <www.terunplay@gmail.com>
2024-05-03 06:28:28 +0300

    Added imported styles

--- src/components/Message/Animation/Animation.tsx
+++ src/components/Message/Animation/Animation.tsx
@@ -1,13 +1,6 @@
 import { FC } from "react";
 import { useAppSelector } from "../../../hooks";
-
-// position: absolute;
-//     top: 0;
-//     left: 0;
-//     bottom: 0;
-//     right: 0;
-//     border: 1px solid #00aeef;
-//     border-radius: 50%;
+import styles from "./Animation.module.css";
 
 const Animation: FC = (): JSX.Element => {
   const { colorTheme } = useAppSelector((state) => state.global);
@@ -15,7 +8,7 @@ const Animation: FC = (): JSX.Element => {
     <>
       <div
         style={{ borderColor: colorTheme }}
-        className="z-10 animate-widgetPulse absolute top-0 left-0 bottom-0 right-0 border-[1px] rounded-[50%] cursor-pointer"
+        className={styles.neurosama}
       ></div>
     </>
   );
