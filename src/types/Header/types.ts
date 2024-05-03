d945451795c74efd413824553bbe622ecc1ea2fb
Ferenandios <www.terunplay@gmail.com>
2024-05-03 07:13:26 +0300

    Deleted useless-pomoika properties

--- src/types/Header/types.ts
+++ src/types/Header/types.ts
@@ -1,15 +1,4 @@
-export type titleType = "Контакты" | "Навигация";
-
-export interface IElement {
-  id: number;
-  text: string;
-  link: string;
-}
-
 export interface IState {
-  contacts: IElement[];
-  navigation: IElement[];
   searchIcon: string;
-  activeElementId: number;
   buttonText: string;
 }
