506e7cb7b537f63cf3d281a91d41bd52dcca5a17
Ferenandios <www.terunplay@gmail.com>
2024-05-03 07:15:11 +0300

    Deleted useless-pomoika properties

--- src/components/Header/Burger/Info/Contacts/Title/Title.tsx
+++ src/components/Header/Burger/Info/Contacts/Title/Title.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
 import styles from "./Title.module.css";
-import { useAppSelector } from "../../../../../../hooks";
 
 const Title: FC = (): JSX.Element => {
-  const { contactsTitle } = useAppSelector((state) => state.footer);
   return (
     <>
-      <h1 className={styles.title}>{contactsTitle}</h1>
+      <h1 className={styles.title}>Контакты</h1>
     </>
   );
 };
