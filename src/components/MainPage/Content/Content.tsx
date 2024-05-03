8188043b4abb77ff881042253defb1bf8d554917
Ferenandios <www.terunplay@gmail.com>
2024-05-03 06:33:32 +0300

    Added imported styles

--- src/components/MainPage/Content/Content.tsx
+++ src/components/MainPage/Content/Content.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import Title from "./Title/Title";
 import Services from "./Services/Services";
+import styles from './Content.module.css'
 
 const Content: FC = (): JSX.Element => {
   return (
     <>
-      <section className="pt-[12px] pb-[18px] lg:pt-[24px]">
-        <div className="container px-[8px] sm:mx-auto">
-          <div className="flex flex-col min-h-[500px] gap-[28px] lg:items-center">
+      <section className={styles.section}>
+        <div className={styles.containerr}>
+          <div className={styles.inner}>
             <Title />
             <Services />
           </div>
