26e35008c9c5f885253a88af1c22cdc9c0f108a0
Ferenandios <www.terunplay@gmail.com>
2024-05-07 06:05:34 +0300

    Changed attribute to for a Link

--- src/components/Footer/Logo/Logo.tsx
+++ src/components/Footer/Logo/Logo.tsx
@@ -4,11 +4,13 @@ import styles from "./Logo.module.css";
 import { Link } from "react-router-dom";
 
 const Logo: FC = (): JSX.Element => {
-  const { companyLogo } = useAppSelector((state) => state.global);
+  const { crumbs, companyLogo } = useAppSelector((state) => state.global);
+  const mainPage: string =
+    crumbs[crumbs.findIndex((crumb) => crumb.link === "/")].link;
   return (
     <>
       <div className={styles.ansoc}>
-        <Link to={"/rome"}>
+        <Link to={mainPage}>
           <img className={styles.img} src={companyLogo} alt="Logo" />
         </Link>
       </div>
--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 
 const Logo: FC = (): JSX.Element => {
-  const { companyLogo } = useAppSelector((state) => state.global);
+  const { crumbs, companyLogo } = useAppSelector((state) => state.global);
+  const mainPage: string =
+    crumbs[crumbs.findIndex((crumb) => crumb.link === "/")].link;
   return (
     <>
-      <Link className={styles.ansoc} to={"/rome"}>
+      <Link className={styles.ansoc} to={mainPage}>
         <img className="h-[30px] lg:h-[50px]" src={companyLogo} alt="Logo" />
       </Link>
     </>
