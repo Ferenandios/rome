8ea431dbac8eb82d69e63d4e1e56fb842fbca191
Ferenandios <www.terunplay@gmail.com>
2024-05-09 05:45:46 +0300

    Fixed to attribute in Link

--- src/components/MainPage/Content/Services/Service/Service.tsx
+++ src/components/MainPage/Content/Services/Service/Service.tsx
@@ -2,15 +2,12 @@ import { FC } from "react";
 import Image from "./Image/Image";
 import Title from "./Title/Title";
 import { Link } from "react-router-dom";
-import styles from './Service.module.css'
+import styles from "./Service.module.css";
 
 const Service: FC<{ serviceId: number }> = ({ serviceId }): JSX.Element => {
   return (
     <>
-      <Link
-        className={styles.inner}
-        to={`/rome/catalog/${serviceId}`}
-      >
+      <Link className={styles.inner} to={`/catalog/${serviceId}`}>
         <Image serivceId={serviceId} />
         <Title serviceId={serviceId} />
       </Link>
