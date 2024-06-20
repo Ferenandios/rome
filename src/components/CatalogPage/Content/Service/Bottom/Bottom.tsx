2d601dfc0776da68a9910f159ce85b54225c7c79
Ferenandios <www.terunplay@gmail.com>
2024-06-20 17:01:04 +0300

    Added Gallery-component

--- src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
+++ src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import styles from "./Bottom.module.css";
 import Description from "./Description/Description";
+import Gallery from "./Gallery/Gallery";
 
 const Bottom: FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.inner}>
         <Description />
+        <Gallery />
       </div>
     </>
   );
