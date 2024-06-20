1f08a8969ded8fc1797a81eb5765f7eb469781fa
Ferenandios <www.terunplay@gmail.com>
2024-06-20 15:21:08 +0300

    Removed Form

--- src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
+++ src/components/CatalogPage/Content/Service/Bottom/Bottom.tsx
@@ -1,26 +1,12 @@
 import { FC } from "react";
-import Description from "./Description/Description";
-import Form from "../../../../Form/Form";
 import styles from "./Bottom.module.css";
-import { useAppSelector } from "../../../../../hooks";
+import Description from "./Description/Description";
 
 const Bottom: FC = (): JSX.Element => {
-  const { description } = useAppSelector(
-    (state) => state.global.currentService
-  );
   return (
     <>
       <div className={styles.inner}>
         <Description />
-        <div style={{ display: description.length > 0 ? "flex" : "none" }}>
-          <Form />
-        </div>
-
-        {description.length === 0 && (
-          <div className="flex lg:hidden">
-            <Form />
-          </div>
-        )}
       </div>
     </>
   );
