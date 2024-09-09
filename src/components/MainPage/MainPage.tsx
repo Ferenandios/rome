24491c8f161e8fc618835bd9ab4f7155ccc2856a
Ferenandios <www.terunplay@gmail.com>
2024-09-09 17:38:02 +0300

    Added description and title meta

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -7,15 +7,23 @@ import Message from "../Message/Message";
 import Crumbs from "../Crumbs/Crumbs";
 import Modal from "../Modal/Modal";
 import styles from "./MainPage.module.css";
+import DocumentMeta from "react-document-meta";
 
 const MainPage: FC = (): JSX.Element => {
-  const { burgerIsOpen, isShowModal } = useAppSelector((state) => state.global);
+  const { burgerIsOpen, isShowModal, companyName } = useAppSelector(
+    (state) => state.global
+  );
+  const meta = {
+    title: `${companyName} - Ваш надежный партнер в Нижневартовске | Услуги компании`,
+    description: `${companyName} - широкий спектр услуг в Нижневартовске. Надежность, профессионализм, качество. Цены от 5 000 руб. Встречайте 2024 год с нами!`,
+  };
   useEffect(() => {
     // Changing Page title by service.title
     document.title = "Услуги";
   });
   return (
     <>
+      <DocumentMeta {...meta} />
       <main className={`${burgerIsOpen || isShowModal ? styles.inner : ""}`}>
         <Header />
         <Crumbs />
