18ce5cd6aa62c7c675e427697c7406ec30cb6bb7
Ferenandios <www.terunplay@gmail.com>
2024-09-10 06:56:15 +0300

    Removed meta tags

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -10,20 +10,13 @@ import styles from "./MainPage.module.css";
 import DocumentMeta from "react-document-meta";
 
 const MainPage: FC = (): JSX.Element => {
-  const { burgerIsOpen, isShowModal, companyName } = useAppSelector(
-    (state) => state.global
-  );
-  const meta = {
-    title: `${companyName} - Ваш надежный партнер в Нижневартовске | Услуги компании`,
-    description: `${companyName} - широкий спектр услуг в Нижневартовске. Надежность, профессионализм, качество. Цены от 5 000 руб. Встречайте 2024 год с нами!`,
-  };
+  const { burgerIsOpen, isShowModal } = useAppSelector((state) => state.global);
   useEffect(() => {
     // Changing Page title by service.title
     document.title = "Услуги";
   });
   return (
     <>
-      <DocumentMeta {...meta} />
       <main className={`${burgerIsOpen || isShowModal ? styles.inner : ""}`}>
         <Header />
         <Crumbs />
