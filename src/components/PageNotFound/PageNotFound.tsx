6fd2872df086f0ef2ec263b2e85aa55d2887c68d
Ferenandios <www.terunplay@gmail.com>
2024-10-01 20:27:49 +0300

    Added title in head

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -5,10 +5,14 @@ import Content from "./Content/Content";
 import Crumbs from "../Crumbs/Crumbs";
 import Message from "../Message/Message";
 import Modal from "../Modal/Modal";
+import { Helmet } from "react-helmet";
+import { useAppSelector } from "../../hooks";
 
 const PageNotFound: FC = (): JSX.Element => {
+  const { companyName } = useAppSelector((state) => state.global);
   return (
     <>
+      <Helmet title={`Страница не найдена - ${companyName}`} />
       <Header />
       <Crumbs />
       <Content />
