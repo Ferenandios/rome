14ae719e35ce0593c1c95b25a5b833be1d411edc
Ferenandios <www.terunplay@gmail.com>
2024-10-01 20:30:31 +0300

    Added meta and title in head

--- src/components/ContactsPage/ContactsPage.tsx
+++ src/components/ContactsPage/ContactsPage.tsx
@@ -1,20 +1,38 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
+import { Helmet } from "react-helmet";
+import { useAppSelector } from "../../hooks";
 import Crumbs from "../Crumbs/Crumbs";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Message from "../Message/Message";
 import Modal from "../Modal/Modal";
-import Content from "./Content/Content";
-import { useAppSelector } from "../../hooks";
 import styles from "./ContactsPage.module.css";
+import Content from "./Content/Content";
 
 const ContactsPage: FC = (): JSX.Element => {
-  const { burgerIsOpen, isShowModal } = useAppSelector((state) => state.global);
-  useEffect(() => {
-    document.title = "Контакты";
-  });
+  const { burgerIsOpen, isShowModal, companyName } = useAppSelector(
+    (state) => state.global
+  );
   return (
     <>
+      <Helmet
+        title={`Контакты - ${companyName}`}
+        meta={[
+          {
+            name: "title",
+            content: `Контакты - ${companyName}`,
+          },
+          {
+            name: "description",
+            content: `${companyName} - широкий спектр услуг в Нижневартовске. Надежность, профессионализм, качество. Цены от 5 000 руб. Встречайте 2024 год с нами!`,
+          },
+          {
+            name: "keywords",
+            content:
+              "ремикс в нижневартовске, ооо ремикс нижневартовск, услуги ремикс, ремикс нв, контакты ремикс, каталог, индустриальная 14 стр 13, remiksnv@yandex.ru, remiksnv, remiks86",
+          },
+        ]}
+      />
       <main className={`${burgerIsOpen || isShowModal ? styles.inner : ""}`}>
         <Header />
         <Crumbs />
